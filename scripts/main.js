380d9c32b5ef8641e0456f9ce6675a1c642d6347
AureLeeLi <aduynslaeger@gmail.com>
2024-06-24 10:27:31 +0200

    💄 ul + li for select options

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,33 @@
 import recipes from "./../data/recipes.js";
-import { addTag, displayRecipes, mainSearch, updateActiveFilters } from "./utils/search.js";
+import { addTag, displayRecipes, mainSearch } from "./utils/search.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Récupération des éléments du DOM
     const recipesContainer = document.getElementById("recipes");
     const inputSearch = document.getElementById("search");
     const searchForm = document.getElementById("search-form");
     const searchIcon = document.getElementById("search-icon");
 
-    // Fonction pour effectuer la recherche et ajouter le tag
     const performSearch = (query) => {
         if (query.length >= 3) {
             mainSearch(query, recipesContainer);
             updateActiveFilters();
         }
-        // Vider le champ de recherche après la soumission, même si la recherche n'a pas atteint 3 caractères
         inputSearch.value = "";
     };
 
-    // Écouter les soumissions du formulaire
     searchForm.addEventListener("submit", (event) => {
         event.preventDefault();
         const query = inputSearch.value.trim();
         performSearch(query);
     });
 
-    // Écouter le clic sur l'icône de recherche
     if (searchIcon) {
-        searchIcon.addEventListener("click", (event) => {
+        searchIcon.addEventListener("click", () => {
             const query = inputSearch.value.trim();
             performSearch(query);
         });
     }
 
-    // Écouter les entrées de l'utilisateur pour les recherches dynamiques
     inputSearch.addEventListener("input", (event) => {
         const query = event.target.value.trim();
         if (query.length >= 3) {
@@ -44,60 +38,65 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    // Écouter les changements dans les selects d'ingrédients, d'ustensiles et d'appareils
-    const ingredientsSelect = document.getElementById("ingredients-select");
-    ingredientsSelect.addEventListener("change", (event) => {
-        const selectedIngredient = event.target.value;
-        if (selectedIngredient) {
-            addTag(selectedIngredient);
-            updateActiveFilters();
-            updateRecipesWithTags();
-        }
-    });
+    const setupCustomSelect = (selectId) => {
+        const selectContainer = document.getElementById(selectId);
+        const title = selectContainer.querySelector(".select-title");
+        const inputContainer = selectContainer.querySelector(".select-input-container");
+        const input = selectContainer.querySelector(".select-input");
+        const options = selectContainer.querySelector(".select-options");
 
-    const appliancesSelect = document.getElementById("appliances-select");
-    appliancesSelect.addEventListener("change", (event) => {
-        const selectedAppliance = event.target.value;
-        if (selectedAppliance) {
-            addTag(selectedAppliance);
-            updateActiveFilters();
-            updateRecipesWithTags();
-        }
-    });
+        title.addEventListener("click", () => {
+            inputContainer.classList.toggle("hidden");
+            options.classList.toggle("hidden");
+            input.focus();
+        });
 
-    const ustensilsSelect = document.getElementById("ustensils-select");
-    ustensilsSelect.addEventListener("change", (event) => {
-        const selectedUstensil = event.target.value;
-        if (selectedUstensil) {
-            addTag(selectedUstensil);
-            updateActiveFilters();
-            updateRecipesWithTags();
-        }
-    });
+        input.addEventListener("input", (event) => {
+            const query = event.target.value.toLowerCase();
+            const allOptions = options.querySelectorAll("li");
+            allOptions.forEach(option => {
+                const text = option.textContent.toLowerCase();
+                if (text.includes(query)) {
+                    option.classList.remove("hidden");
+                } else {
+                    option.classList.add("hidden");
+                }
+            });
+        });
 
-    // Mettre à jour les filtres actifs
+        options.addEventListener("click", (event) => {
+            if (event.target.tagName === "LI") {
+                const selectedOption = event.target.textContent;
+                addTag(selectedOption);
+                updateActiveFilters();
+                updateRecipesWithTags();
+                inputContainer.classList.add("hidden");
+                options.classList.add("hidden");
+                input.value = "";
+            }
+        });
+    };
+
+    setupCustomSelect("ingredients-select");
+    setupCustomSelect("appliances-select");
+    setupCustomSelect("ustensils-select");
+
     function updateActiveFilters() {
         const tagsContainer = document.getElementById("tags");
         const tags = Array.from(tagsContainer.children).map(
             (element) => element.querySelector("p").textContent
         );
-        // Appeler la fonction pour mettre à jour les filtres avec les tags actifs
         updateFilters(tags);
     }
 
-    // Fonction pour mettre à jour les filtres avec les tags actifs
     function updateFilters(tags) {
-        // Implémenter la logique pour mettre à jour les filtres (à adapter selon votre structure HTML et votre application)
         console.log("Filtres mis à jour :", tags);
     }
 
-    // Effacer les filtres actifs
     function clearActiveFilters() {
-        // Implémenter la logique pour effacer les filtres actifs (à adapter selon votre structure HTML et votre application)
         console.log("Filtres effacés");
     }
 
-    //mise à jour les recettes en fonction des tags actifs
     function updateRecipesWithTags() {
         const tagsContainer = document.getElementById("tags");
         const tags = Array.from(tagsContainer.children).map(
@@ -107,6 +106,17 @@ document.addEventListener("DOMContentLoaded", () => {
         mainSearch(query, recipesContainer);
     }
 
-    // Afficher les recettes par défaut
     displayRecipes(recipes, recipesContainer);
 });
+
+function updateSelectOptions(selectId, options) {
+    const selectElement = document.querySelector(`#${selectId} .select-options`);
+    selectElement.innerHTML = "";
+
+    options.forEach(option => {
+        const optionElement = document.createElement("li");
+        optionElement.classList.add("py-4", "hover:bg-customYellow", "cursor-pointer");
+        optionElement.textContent = option;
+        selectElement.appendChild(optionElement);
+    });
+}
--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -6,13 +6,11 @@ let currentFilters = [];
 
 export function mainSearch(query, container) {
     if (query.length < 3 && currentFilters.length === 0) {
-        // Si la requête n'atteint pas 3 caractères, on affiche toutes les recettes
         displayRecipes(recipes, container);
         updateRecipeCount(recipes.length);
         return;
     }
 
-    // Filtrage des recettes
     const filteredRecipes = recipes.filter(recipe => {
         const queryLower = query.toLowerCase();
         const nameMatch = recipe.name.toLowerCase().includes(queryLower);
@@ -21,7 +19,6 @@ export function mainSearch(query, container) {
             ingredient.ingredient.toLowerCase().includes(queryLower)
         );
 
-        // Vérifier si toutes les conditions des filtres actifs sont remplies
         const allFiltersMatch = currentFilters.every(filter =>
             recipe.ingredients.some(ingredient =>
                 ingredient.ingredient.toLowerCase().includes(filter.toLowerCase())
@@ -31,14 +28,11 @@ export function mainSearch(query, container) {
         return nameMatch || descriptionMatch || ingredientsMatch;
     });
 
-    // Affichage des recettes filtrées
     displayRecipes(filteredRecipes, container);
     updateRecipeCount(filteredRecipes.length);
-    // Mise à jour des filtres de recherche avancée
     updateFilters(filteredRecipes);
 }
 
-// Fonction pour mettre à jour les filtres actifs
 export function updateActiveFilters(tags) {
     currentFilters = tags;
 }
@@ -49,18 +43,15 @@ export function displayRecipes(recipesFound, container) {
         const cardHTML = recipeCard(recipe);
         container.innerHTML += cardHTML;
     });
-    // Afficher le nombre de recettes
     const countRecipe = document.getElementById("recipe-count");
     countRecipe.textContent = `${recipesFound.length} recette(s)`;
 }
 
-// Fonction pour mettre à jour le nombre de recettes affichées
 function updateRecipeCount(count) {
     const countRecipe = document.getElementById("recipe-count");
     countRecipe.textContent = `${count} recette(s)`;
 }
 
-// Fonction pour mettre à jour les filtres select ingrédients/ustensiles/appareils
 function updateFilters(filteredRecipes) {
     let ingredients = [];
     let appliances = [];
@@ -77,51 +68,42 @@ function updateFilters(filteredRecipes) {
     appliances = [...new Set(appliances)];
     ustensils = [...new Set(ustensils)];
 
-    updateSelect("ingredients-select", ingredients);
-    updateSelect("appliances-select", appliances);
-    updateSelect("ustensils-select", ustensils);
+    updateSelectOptions("ingredients-select", ingredients);
+    updateSelectOptions("appliances-select", appliances);
+    updateSelectOptions("ustensils-select", ustensils);
 }
 
-// Fonction pour mettre à jour les options de chaque select
-function updateSelect(selectId, options) {
-    const selectElement = document.getElementById(selectId);
-    selectElement.innerHTML = selectElement.querySelector('option[value=""]').outerHTML;
+function updateSelectOptions(selectId, options) {
+    const selectElement = document.querySelector(`#${selectId} .select-options`);
+    selectElement.innerHTML = "";
 
     options.forEach(option => {
-        const optionElement = document.createElement("option");
+        const optionElement = document.createElement("li");
+        optionElement.classList.add("p-4", "hover:bg-customYellow", "cursor-pointer");
         optionElement.textContent = option;
         selectElement.appendChild(optionElement);
     });
 }
 
-// Fonction pour ajouter un tag de recherche
 export function addTag(tag) {
-    //on cible l'élement container du dom où afficher les tags
     const tagsContainer = document.getElementById("tags");
-    //on cible l'élement enfant de ce container qui recevra le tag
     const existingTag = Array.from(tagsContainer.children).find(
         (element) => element.querySelector("p").textContent === tag
     );
 
-    //si le tag n'existe pas
     if (!existingTag) {
-        //on utilise la fonction d'affichge du tag de la recherche
         const tagHTML = searchTag(tag);
         tagsContainer.innerHTML += tagHTML;
 
-        // Ajout de l'événement de suppression du tag
         const tagElements = tagsContainer.querySelectorAll(".tag");
         const lastTagElement = tagElements[tagElements.length - 1];
-        //on cible l'icone pour supprimer le tag
         lastTagElement.querySelector("i").addEventListener("click", () => {
             lastTagElement.remove();
-            //mise à jour la recherche après la suppression du tag
             updateSearch();
         });
     }
 }
 
-//Fonction pour mettre à jour la recherche après la suppression d'un tag
 function updateSearch() {
     const tagsContainer = document.getElementById("tags");
     const tags = Array.from(tagsContainer.children).map(
