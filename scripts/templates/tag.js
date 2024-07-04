e80a43d566ddfd74ff5afb53bdde2a5a14da0922
AureLeeLi <aduynslaeger@gmail.com>
2024-07-04 12:44:12 +0200

    🚧 work on tags : cursor, recipes display, hide ul on click

--- scripts/templates/tag.js
+++ scripts/templates/tag.js
@@ -2,7 +2,7 @@ export function searchTag(selectedTag) {
     return `
         <div class="tag bg-customYellow rounded-lg font-manrope flex justify-evenly items-center p-4 gap-3">
             <p>${selectedTag}</p>
-            <i class="fa-solid fa-xmark text-black"></i>
+            <i class="fa-solid fa-xmark text-black cursor-pointer"></i>
         </div>
     `;
 }
