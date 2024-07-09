55317397ddea6c6d01ccd2ba983f367beccc348a
AureLeeLi <aduynslaeger@gmail.com>
2024-07-09 11:28:55 +0200

    ✅ xss function fix

--- scripts/search/searchFunctions.js
+++ scripts/search/searchFunctions.js
@@ -4,13 +4,28 @@ import { setupDropdown } from "./filterFunctions.js";
 
 let searchResults = [];
 
+function escapeHtml(text) {
+    //caractères spéciaux HTML à leurs équivalents échappés
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    //recherche tous les caractères dans le texte
+    //puis remplace chaque occurence trouvée par la valeur correspondante
+    return text.replace(/[&<>"']/g, function (m) { return map[m]; });
+}
+
 // Fonction principale pour effectuer la recherche
 export function mainSearch(query, container) {
+    const checkedInput = escapeHtml(query);
     searchResults = [];
 
     // Filtrer les recettes en fonction de la recherche principale (query)
-    if (query.length >= 3) {
-        const queryLower = query.toLowerCase();
+    if (checkedInput.length >= 3) {
+        const queryLower = checkedInput.toLowerCase();
 
         // Itérer sur toutes les recettes
         for (let i = 0; i < recipes.length; i++) {
